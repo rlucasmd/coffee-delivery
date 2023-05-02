a005a1e90eb6e203f6bb63a0f8365a6fd9bb3af7
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-01 21:51:27 -0300

    add: price calculated with reducer array

--- src/pages/Checkout/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/Checkout/components/SelectedCoffees/ConfirmationSection.tsx
@@ -1,21 +1,30 @@
 import { Button } from "../../../../components/Button";
 import { Text } from "../../../../components/Typography";
+import { useCart } from "../../../../hooks/useCart";
+import { formatMoneyToBRL } from "../../../../utils/formatMoney";
 import { ConfirmationSectionContainer } from "./styles";
 
 function ConfirmationSection(){
+  const { cart } = useCart();
+  const totalProductsPrice = cart.reduce((accumulator, coffee) => {
+    accumulator += coffee.price * coffee.quantity;
+    return accumulator;
+  }, 0);
+  const deliveryTax = 10;
+  const totalPrice = totalProductsPrice + deliveryTax;
   return (
     <ConfirmationSectionContainer>
       <div>
         <Text size="s">Total de itens</Text>
-        <Text size="s">R$ 29,00</Text>
+        <Text size="s">{`R$ ${formatMoneyToBRL(totalProductsPrice)}`}</Text>
       </div>
       <div>
         <Text size="s">Entrega</Text>
-        <Text size="s">R$ 10,00</Text>
+        <Text size="s">{`R$ ${formatMoneyToBRL(deliveryTax)}`}</Text>
       </div>
       <div>
         <Text weight={700} size="l" color="title">Total de itens</Text>
-        <Text weight={700} size="l" color="title">R$ 39,00</Text>
+        <Text weight={700} size="l" color="title">{`R$ ${formatMoneyToBRL(totalPrice)}`}</Text>
       </div>
       <Button title="confirmar pedido"/>
     </ConfirmationSectionContainer>
